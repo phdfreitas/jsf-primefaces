404c5df074529d3cca2e3329da7f77e23d977bc5
phdfreitas <pedro.hnd.freitas@gmail.com>
2024-03-29 08:27:59 -0300

    Creating first entities and setting up persistence.xml

---
+++ src/main/java/com/jsf/primefaces/erp/enums/TipoEmpresa.java
@@ -0,0 +1,21 @@
+package com.jsf.primefaces.erp.enums;
+
+public enum TipoEmpresa {
+	
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+	
+	TipoEmpresa(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+
+}
